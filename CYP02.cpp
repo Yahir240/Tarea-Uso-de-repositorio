7272733aa96d31862f113dedad930b83a1be2d5e
EderLp56 <ederlopez3070@gmail.com>
2024-09-24 21:00:41 -0600

    Update CYP02.cpp

--- CYP02.cpp
+++ CYP02.cpp
@@ -5,14 +5,5 @@ int main()
     int bfSize, bfReserved1, bfReserved2, bfOffBits, bfType1, bfType2;
     scanf_s("%i%i", &bfSize, &bfOffBits);
 
-    bfType1 = 66;      // 'B'
-    bfType2 = 77;      // 'M'
-    bfReserved1 = 0;   // Valor por defecto
-    bfReserved2 = 0;   // Valor por defecto
-    printf("%c%c%08X%04X%04X%08X", bfType1, bfType2, bfSize, bfReserved1, bfReserved2, bfOffBits);
 
-    return 0;
-}
-
-//Tarea de repositorio colaborativo 
 
