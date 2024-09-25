cb08d44d7a1b04ad4210a4b411d7a8c6553f9e23
Yahir240 <yahirho@hotmail.com>
2024-09-24 21:01:52 -0600

    Update CYP02.cpp

--- CYP02.cpp
+++ CYP02.cpp
@@ -6,4 +6,12 @@ int main()
     scanf_s("%i%i", &bfSize, &bfOffBits);
 
 
+bfType1 = 66;      // 'B'
+    bfType2 = 77;      // 'M'
+    bfReserved1 = 0;   // Valor por defecto
+    bfReserved2 = 0;   // Valor por defecto
+    printf("%c%c%08X%04X%04X%08X", bfType1, bfType2, bfSize, bfReserved1, bfReserved2, bfOffBits);
+
+    return 0;
+}
 
